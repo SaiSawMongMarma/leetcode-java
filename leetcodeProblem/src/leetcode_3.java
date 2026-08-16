776aa8824583f0ffb0d861342fdabd05460df996
sai saw mong <saisawmong0@gmail.com>
2026-08-16 16:30:54 +0600

    ok

--- leetcodeProblem/src/leetcode_3.java
+++ leetcodeProblem/src/leetcode_3.java
@@ -15,7 +15,22 @@ public class leetcode_3 {
 
         for(int right = 0; right<s.length(); right++){
 
+//            If duplicate exist, remove characters
+//            From the left until duplicate is gone
+
+            while(set.contains(s.charAt(right))){
+                set.remove(s.charAt(left));
+                left++;
+            }
+//            Add current character
+            set.add(s.charAt(right));
+
+//            Calculate current window length
+            int length = right - left + 1;
+            maxLength = Math.max(maxLength, length);
         }
+
+        return maxLength;
     }
 
     public static void main(String[] args) {
