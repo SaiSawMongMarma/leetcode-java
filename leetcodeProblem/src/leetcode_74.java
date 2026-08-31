0abf951a74207a9a499436117f619662349703d6
sai saw mong <saisawmong0@gmail.com>
2026-08-31 06:33:45 +0600

    ok

--- leetcodeProblem/src/leetcode_74.java
+++ leetcodeProblem/src/leetcode_74.java
@@ -1,13 +1,21 @@
+//Search a 2D Matrix
+//m=3 (row)
+//n=4 (column)
+//1D Array as 2D Matrix solving by using Binary Search
+
 public class leetcode_74 {
     public boolean searchMatrix(int[][] matrix, int target) {
         int m = matrix.length;
         int n = matrix[0].length;
 
         int left = 0;
+//        last position
         int right = m * n - 1;
 
+//      Using Binary Search
         while (left <= right) {
             int mid = left + (right - left) / 2;
+//            finding value in 2D Array
             int midValue = matrix[mid / n][mid % n];
 
             if (midValue == target) {
