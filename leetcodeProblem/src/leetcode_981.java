a65edb958ef9033dc8d53b9f17bff19efa5962ca
sai saw mong <saisawmong0@gmail.com>
2026-09-01 08:06:40 +0600

    ok

---
+++ leetcodeProblem/src/leetcode_981.java
@@ -0,0 +1,29 @@
+//Time Based Key-Value Store
+/*
+ * Your TimeMap object will be instantiated and called as such:
+ * TimeMap obj = new TimeMap();
+ * obj.set(key,value,timestamp);
+ * String param_2 = obj.get(key,timestamp);
+ */
+
+import java.util.*;
+public class leetcode_981 {
+    private Map<String, TreeMap<Integer, String>> map;
+
+    public leetcode_981() {
+        map = new HashMap<>();
+    }
+
+    public void set(String key, String value, int timestamp) {
+        map.computeIfAbsent(key, k -> new TreeMap<>()).put(timestamp, value);
+    }
+
+    public String get(String key, int timestamp) {
+        TreeMap<Integer, String> treeMap = map.get(key);
+        if (treeMap == null) {
+            return "";
+        }
+        Map.Entry<Integer, String> entry = treeMap.floorEntry(timestamp);
+        return entry == null ? "" : entry.getValue();
+    }
+}
